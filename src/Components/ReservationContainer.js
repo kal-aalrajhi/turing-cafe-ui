87e13609c2a4755696057ed51aabd3345d1f3294
Kal <kal.aalrajhi@gmail.com>
2022-05-25 10:04:57 -0600

    Populate reservation cards with props

--- src/Components/ReservationContainer.js
+++ src/Components/ReservationContainer.js
@@ -3,7 +3,18 @@ import ReservationCard from './ReservationCard';
 import '../Styles/ReservationContainer.css'
 
 const ReservationContainer = ({ reservations }) => {
-    console.log(reservations)
+    const reservationCards = reservations.map(reservation => {
+        return (
+            <ReservationCard
+                name={reservation.name}
+                date={reservation.date}
+                time={reservation.time}
+                guestCount={reservation.number}
+                id={reservation.id}
+                key={reservation.id}
+            />
+        )
+    })
     return (
         <></>
     );
