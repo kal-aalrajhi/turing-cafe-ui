9dfe86b032518bed159ca012f2ef0cab53e5e486
Kal <kal.aalrajhi@gmail.com>
2022-05-25 10:09:46 -0600

    Display cards without styling

--- src/Components/ReservationCard.js
+++ src/Components/ReservationCard.js
@@ -4,7 +4,10 @@ import '../Styles/ReservationCard.css'
 const ReservationCard = ({name, date, time, guestCount, id}) => {
     return (
         <div className='card'>
-            
+            <h2>{name}</h2>
+            <h3>{date}</h3>
+            <h3>{time}</h3>
+            <p>{guestCount}</p>
         </div>
     );
 }
